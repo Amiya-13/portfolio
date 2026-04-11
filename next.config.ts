cf68f87fb4109f856c989d471b9b8cce5eac301e
Amiya <amiya.mishu2023@iem.edu.in>
2026-04-11 13:58:38 +0530

    config error fixed

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
   devIndicators: {
-    appIsrStatus: false,
     buildActivity: false,
   },
 };
