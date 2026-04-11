b7629973fe6278046e135539f037f1bf72af5690
Amiya <amiya.mishu2023@iem.edu.in>
2026-04-11 14:06:29 +0530

    Remove broken devIndicators locally

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
-  devIndicators: {
-    appIsrStatus: false,
-  },
 };
 
 export default nextConfig;
