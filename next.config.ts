62e8fe54cc4757d0bace32acd66e0e974c0ee208
Amiya <amiya.mishu2023@iem.edu.in>
2026-04-11 14:02:36 +0530

    dep issue

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
   devIndicators: {
-    buildActivity: false,
+    appIsrStatus: false,
   },
 };
 
